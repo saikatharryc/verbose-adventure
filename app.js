9a6b3abd5b9196cc2abd2e89c3f67b5527f52a11
saikatharryc <saikatchakrabortty2@gmail.com>
2017-06-16 16:00:42 +0530

    Added Swing View Engine

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const swig = require('swig');
 const routes = require('./app/controllers');
 const db_connect = require('./lib/db_connect')();
 
@@ -21,6 +21,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/*******************************************************
+    VIEW ENGINE & PATH
+********************************************************/
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
 
 /*******************************************************
     BASE API ENDPOINT
