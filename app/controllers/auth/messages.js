ba33022fcc4b42fa3c4f0293b6d065cb88e9c07a
saikatharryc <saikatchakrabortty2@gmail.com>
2017-06-16 15:18:16 +0530

    Partially build The Thread save system

--- app/controllers/auth/messages.js
+++ app/controllers/auth/messages.js
@@ -8,8 +8,16 @@ const async = require('async');
 const router = express.Router();
 let threadIds;
 let store;
+const samary = [];
 
 
+/**
+ * List all the threadIds
+ *
+ * @param      {object}    req     The request
+ * @param      {object}    res     The resource
+ * @param      {Function}  next    The next
+ */
 function messages(req, res, next) {
   console.log(access.accessToken);
   if (!access.accessToken) {
@@ -27,25 +35,29 @@ function messages(req, res, next) {
       if (error) {
         console.log(error);
         res.send(error);
-      } else if (JSON.parse(body).nextPageToken){
+      } else if (JSON.parse(body).nextPageToken) {
         store = JSON.parse(body).messages;
+        //pass to the nextPageSystem() again if nextPageToken is available
         nextPageSystem(JSON.parse(body).nextPageToken);
-      }
-      else{
+      } else {
         const messages = JSON.parse(body).messages;
-        //threadIds = messages.map(function (a) { return a.threadId; });
+        threadIds = messages.map(function (a) { return a.threadId; });
 
         //console.log(threadIds.length);
-        res.send(messages);
+        res.send(threadIds);
       }
     });
   }
 
-
-function nextPageSystem(nextPageToken) {
-  const option = {
+/**
+ * Takes nextPageToken as input ,process and concats the result with the previous.
+ *
+ * @param      {string}  nextPageToken  The next page token
+ */
+  function nextPageSystem(nextPageToken) {
+    const option = {
       method: 'GET',
-      url: `${CONFIG.api_base}/gmail/v1/users/me/messages?q="in: newer_than:350d"&maxResults=300&pageToken=`+nextPageToken,
+      url: `${CONFIG.api_base}/gmail/v1/users/me/messages?q="in: newer_than:31d"&maxResults=300&pageToken=${nextPageToken}`,
       headers: {
         'content-type': 'application/x-www-form-urlencoded',
         'Authorization': `Bearer ${access.accessToken}`,
@@ -54,26 +66,28 @@ function nextPageSystem(nextPageToken) {
 
 
     request(option, function (error, response, body) {
-    if (error) {
-      console.log(error);
-    } else if (JSON.parse(body).nextPageToken){
+      if (error) {
+        console.log(error);
+      } else if (JSON.parse(body).nextPageToken) {
         store = store.concat(JSON.parse(body).messages);
         nextPageSystem(JSON.parse(body).nextPageToken);
-      }else {
-        //res.send(body);
-      store = store.concat(JSON.parse(body).messages);
-      console.log({count: store.length})
-    res.send(store);
-    }
-  });
-
+      } else {
+        store = store.concat(JSON.parse(body).messages);
+        console.log({ count: store.length });
+        threadIds = store.map(function (a) { return a.threadId; });
+        res.send(threadIds);
+      }
+    });
+  }
 }
 
-
-}
-
-
-function threadById(thrdId) {
+/**
+ * Get thread Details By ID
+ *
+ * @param      {ObjectId}    thrdId    The thrd identifier
+ * @param      {Function}  callback  The callback
+ */
+function threadById(thrdId, callback) {
   const option = {
     method: 'GET',
     url: `${CONFIG.api_base}/gmail/v1/users/me/threads/${thrdId}?format=minimal`,
@@ -87,29 +101,42 @@ function threadById(thrdId) {
       console.log(error);
     } else {
       const thread = JSON.parse(body);
-      threadLib.saveThread(thread, function (errorInSave, savedThreadInstance) {
-        if (errorInSave) {
-          console.log(errorInSave);
-        }
-        console.log(`${thrdId} saved`);
-      });
+      callback(null, thread);
     }
   });
 }
 
-
+/**
+ * Helps to save into DB , Foreach item in array
+ *
+ * @param      {object}    req     The request
+ * @param      {object}    res     The resource
+ * @param      {Function}  next    The next
+ */
 function babelThreads(req, res, next) {
- var count = 0
-  async.forEach(threadIds, function (elementOfArray, callback) {
-    console.log(elementOfArray+' '+ count);
-    threadById(elementOfArray);
-    count++
-    callback()
-  }, function (err) {
-    if (err) { throw err; }
-    console.log('processing all elements completed');
+  count = 0;
+  async.each(threadIds, function (data) {
+    threadById(data, function (error, result) {
+      if (error) {
+        return error;
+      }
+      console.log(count);
+      //Concates result and makes an array of all the objects to be saved
+      samary.push(result);
+      count++;
+      // Stop and Start processing when the element of an array processed, pyshed to summary.
+      if (count === threadIds.length) {
+        threadLib.saveThread(samary, function (errorInSave, savedThreadInstance) {
+          if (errorInSave) {
+            res.send(errorInSave);
+          }
+          res.send('all data saved');
+        });
+      }
+    });
   });
 }
+
 
 router.get('/messages', messages);
 router.get('/thread', babelThreads);
